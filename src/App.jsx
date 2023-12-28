a1dddaf294dadc00d837940b49a169045a98a202
nijesmik <bkybb@duck.com>
2023-12-28 12:24:29 +0900

    feat(53): State 관리 및 Hooks 사용법

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
+	const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+
 	function handleSelect(selectedButton) {
-		console.log(selectedButton);
+		setSelectedTopic(selectedButton);
+		console.log(selectedTopic);
 	}
+
+	console.log("APP COMPONENT EXECUTING");
 
 	return (
 		<div>
@@ -35,7 +42,7 @@ function App() {
 						<TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
 						<TabButton onSelect={() => handleSelect("state")}>State</TabButton>
 					</menu>
-					Dynamic Content
+					{selectedTopic}
 				</section>
 			</main>
 		</div>
