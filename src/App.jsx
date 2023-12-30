f8ad032f89d2e1ffbbc3566ea2b768d04675eebe
nijesmik <bkybb@duck.com>
2023-12-30 10:02:24 +0900

    feat(61): Fragments 사용법

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
@@ -31,7 +31,7 @@ function App() {
 	}
 
 	return (
-		<div>
+		<Fragment>
 			<Header />
 			<main>
 				<section id="core-concepts">
@@ -73,7 +73,7 @@ function App() {
 					{tabContent}
 				</section>
 			</main>
-		</div>
+		</Fragment>
 	);
 }
 
