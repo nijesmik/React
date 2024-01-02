229a74fbafce4f6ce7fe8288bfb0f73520a86ca1
nijesmik <bkybb@duck.com>
2024-01-02 18:55:14 +0900

    feat(94): State 끌어올리기 대안

--- 04_EssentialsDeepDive/src/App.jsx
+++ 04_EssentialsDeepDive/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -101,11 +101,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && (
