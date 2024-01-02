0095517f7f4677a6e0346b6a03b4616afd29c3e1
nijesmik <bkybb@duck.com>
2024-01-02 18:43:53 +0900

    feat(93): State를 끌어올리면 안 되는 경우

--- 04_EssentialsDeepDive/src/App.jsx
+++ 04_EssentialsDeepDive/src/App.jsx
@@ -23,6 +23,11 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2",
+  });
+
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
@@ -77,6 +82,15 @@ function App() {
 
   function handleRestart() {
     setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
   }
 
   return (
