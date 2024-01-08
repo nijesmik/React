f585477ae48978a4805c2ef872bb92842da8103f
nijesmik <bkybb@duck.com>
2024-01-08 15:18:05 +0900

    feat(140): State를 사용한 사용자 입력 관리 (양방향 바인딩)

--- 08_Refs_Portals/src/components/Player.jsx
+++ 08_Refs_Portals/src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [playerName, setPlayerName] = useState('unknown entity');
+
+  const handleChange = (event) => {
+    setEnteredPlayerName(event.target.value);
+  };
+
+  const handleClick = () => {
+    if (enteredPlayerName) {
+      setPlayerName(() => enteredPlayerName);
+    }
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
