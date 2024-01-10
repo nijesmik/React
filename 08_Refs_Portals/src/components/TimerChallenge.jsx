e2b201b407c00b1cf52a4c8cc9c38d3d3d047b9a
nijesmik <bkybb@duck.com>
2024-01-10 13:55:04 +0900

    feat(152): 컴포넌트 간의 State 공유

--- 08_Refs_Portals/src/components/TimerChallenge.jsx
+++ 08_Refs_Portals/src/components/TimerChallenge.jsx
@@ -12,9 +12,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
     dialog.current.open();
   }
+
+  const handleReset = () => {
+    setTimeRemaining(targetTime * 1000);
+  };
 
   const handleStart = () => {
     timer.current = setInterval(() => {
@@ -31,7 +34,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
