35aeff04e2f06b6492cac51496027f5fc3242075
nijesmik <bkybb@duck.com>
2024-01-21 08:34:22 +0900

    feat(541): 페이지 간 연결하기

--- 25_NextJS_Introduction/app/news/page.jsx
+++ 25_NextJS_Introduction/app/news/page.jsx
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+
 export default function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 }
