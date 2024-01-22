7cd7c9a4de628446af5aafa65202f29aed445747
nijesmik <bkybb@duck.com>
2024-01-22 21:03:01 +0900

    feat(546): 레이아웃 감싸기

--- 26_NextJS_Project/src/app/layout.js
+++ 26_NextJS_Project/src/app/layout.js
@@ -1,7 +1,14 @@
+import MainNavigation from '@/components/layout/MainNavigation';
+import classes from '@/components/layout/Layout.module.css';
+import '@/styles/globals.css'
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MainNavigation />
+        <main className={classes.main}>{children}</main>
+      </body>
     </html>
   );
 }
